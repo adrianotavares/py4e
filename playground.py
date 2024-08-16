67d40d2c4e1f99447614fdd879e2f1f3f7455050
Adriano Tavares <adriano.tavares@gmail.com>
2024-08-16 17:08:40 -0300

    more Database

--- playground.py
+++ playground.py
@@ -595,36 +595,39 @@
 # 4B617365793332
 # 53616D793238
 
+# Database - SQLLite
 import sqlite3
-
-conn = sqlite3.connect('./db/emaildb.sqlite')
+conn = sqlite3.connect('./db/orgdb.sqlite')
 cur = conn.cursor()
 
-cur.execute('DROP TABLE IF EXISTS Counts')
+cur.execute('SELECT count FROM Counts WHERE org = ? ', ('xxx', ))
+row = cur.fetchone()
+print(type(row))
+print(row)
 
-cur.execute('''
-CREATE TABLE Counts (email TEXT, count INTEGER)''')
+# cur.execute('DROP TABLE IF EXISTS Counts')
+# cur.execute('CREATE TABLE Counts (email TEXT, count INTEGER)')
 
-fname = input('Enter file name: ')
-if len(fname) < 1 :
-    fname = 'mbox-short.txt'
-fh = open(fname)
-for line in fh :
-    if not line.startswith('From: ') :
-        continue
-    pieces = line.split()
-    email = pieces[1]
-    cur.execute('SELECT count FROM Counts WHERE email = ?', (email,))
-    row = cur.fetchone()
-    if row is None :
-        cur.execute('INSERT INTO Counts (email, count) VALUES (?, 1)', (email,))
-    else :
-        cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?', (email,))
-    conn.commit()
+# fname = input('Enter file name: ')
+# if len(fname) < 1 :
+#     fname = 'mbox-short.txt'
+# fh = open(fname)
+# for line in fh :
+#     if not line.startswith('From: ') :
+#         continue
+#     pieces = line.split()
+#     email = pieces[1]
+#     cur.execute('SELECT count FROM Counts WHERE email = ?', (email,))
+#     row = cur.fetchone()
+#     if row is None :
+#         cur.execute('INSERT INTO Counts (email, count) VALUES (?, 1)', (email,))
+#     else :
+#         cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?', (email,))
+#     conn.commit()
     
-sqlstr = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
-
-for row in cur.execute(sqlstr) :
-    print(str(row[0]), row[1])
+# sqlstr = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
+# for row in cur.execute(sqlstr) :
+#     print(str(row[0]), row[1])
     
+
      
