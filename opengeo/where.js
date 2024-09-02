3dd806c8514c4862cb30bfb2a2f8cc0a055409d7
Adriano Tavares <adriano.tavares@gmail.com>
2024-09-02 09:52:22 -0300

    Open Geo

---
+++ opengeo/where.js
@@ -0,0 +1,8 @@
+myData = [
+[-19.87317625,-43.97385326636239, 'Pampulha Campus of the Federal University of Minas Gerais'],
+[-23.288194,-46.750092, 'Pampulha Campus of the Federal University of Minas Gerais'],
+[-18.2410024,-43.6025669089965, 'Universidade do Estado de Minas Gerais'],
+[-19.970813,-43.963581, 'University Center of Belo Horizonte'],
+[-19.9655845,-43.9540617, 'Centro Universitário UNA'],
+[-19.9436953,-43.92540582070728, 'FUMEC University']
+];
