6325fab5c5fb3dc405350feb0f171110b299680f
Adriano Tavares <adriano.tavares@gmail.com>
2024-09-04 12:27:52 -0300

    update

--- playground.py
+++ playground.py
@@ -596,14 +596,14 @@
 # 53616D793238
 
 # Database - SQLLite
-import sqlite3
-conn = sqlite3.connect('./db/orgdb.sqlite')
-cur = conn.cursor()
+# import sqlite3
+# conn = sqlite3.connect('./db/orgdb.sqlite')
+# cur = conn.cursor()
 
-cur.execute('SELECT count FROM Counts WHERE org = ? ', ('xxx', ))
-row = cur.fetchone()
-print(type(row))
-print(row)
+# cur.execute('SELECT count FROM Counts WHERE org = ? ', ('xxx', ))
+# row = cur.fetchone()
+# print(type(row))
+# print(row)
 
 # cur.execute('DROP TABLE IF EXISTS Counts')
 # cur.execute('CREATE TABLE Counts (email TEXT, count INTEGER)')
@@ -629,5 +629,75 @@ print(row)
 # for row in cur.execute(sqlstr) :
 #     print(str(row[0]), row[1])
     
+# x = -1    
+  
+# if x < 2 :
+#     print("Below 2")
+# elif x < 20 :
+#     print("Below 20")
+# elif x < 10 :
+#     print("Below 10")
+# else :
+#     print("Something else")
+       
+# if x < 2 :
+#     print("Below 2")
+# elif x < 0 :
+#     print("Negative")
+# else :
+#     print("Something else")
 
-     
+# zap = "hello there bob"
+# print(zap[4])
+
+# x = 12
+# if x < 5:
+# print("smaller")
+# else:
+#     print("bigger")
+# print("all done")
+
+# def fred():
+#    print("Zap")
+
+# def jane():
+#    print("ABC")
+
+# jane()
+# fred()
+# jane()
+
+# stuff = ['joseph', 'sally', 'walter', 'tim']
+# print(stuff[2])
+
+# def hello():
+#     print("Hello")
+#     print("There")
+
+# x = 10
+# x = x + 1
+
+# x = -1
+# for value in [3, 41, 12, 9, 74, 15] :
+#     if value > x :
+#         x = value
+# print(x)
+
+# total = 0
+# for abc in range(5):
+#     total = total + abc
+#     print(total)
+# print('total:', total)
+
+# a = "123"
+# b = 456
+# c = a + b
+# print(c)
+
+# abc = "With three words"
+# stuff = abc.split()
+# print(stuff)
+
+# abc = "With three words"
+# stuff = abc.split()
+# print(len(stuff))
